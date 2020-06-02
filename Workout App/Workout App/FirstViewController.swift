40cfaeed72b09c4702b83bec37a0bb31a32d1450
danmullasseril <dpm7@rice.edu>
2020-06-02 18:16:37 -0500

    start of the workout app project

---
+++ Workout App/Workout App/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Workout App
+//
+//  Created by Dan Mullasseril on 6/2/20.
+//  Copyright © 2020 Gabe Dan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
