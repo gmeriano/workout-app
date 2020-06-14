59f4d4aa3c4ff12b5ba383cd17eaae3e86a5ea6e
Gabriel Meriano <gvmeriano@gmail.com>
2020-06-14 18:52:49 -0500

    made a stackview and an exercise view class

---
+++ Workout App/Workout App/ExerciseView.swift
@@ -0,0 +1,23 @@
+//
+//  ExerciseView.swift
+//  Workout App
+//
+//  Created by Rita Meriano on 6/14/20.
+//  Copyright © 2020 Gabe Dan. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+
+}
